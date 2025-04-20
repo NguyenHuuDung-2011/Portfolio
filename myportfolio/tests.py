45dfcc7c907e0cca207b84e3620d068c487232a3
NguyenHuuDung-2011 <huudung5c@gmail.com>
2025-04-20 08:57:26 +0700

    .

--- myportfolio/tests.py
+++ myportfolio/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class HomePageTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200) # Check if the status code is 200 (OK)
